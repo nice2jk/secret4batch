8b25e651d5db707a3d18d749182f4cedf981e4d2
nice2jk <deunk32@gmail.com>
2022-12-23 14:29:29 +0900

    add lee 2022

--- Soccer.py
+++ Soccer.py
@@ -54,6 +54,58 @@ def epl(db):
         now=time
         print(now.strftime('%Y-%m-%d %H:%M:%S') + " | request exception | " + e)
         
+def mayo(db):
+    url="https://kr.soccerway.com/teams/spain/real-club-deportivo-mallorca/2027/matches/"
+    
+    try:
+        headers={"User-Agent":"Mozilla/5.0"}
+        req=requests.get(url, headers=headers)
+
+        if req.status_code == 200:
+            html=req.text
+            soup=bs(html, "html.parser")
+            bsi=soup.select("tr.match")
+            # print(bsi)            
+            
+            my_list = list()
+            for data in bsi:
+                # print(data)
+                try:
+                    matchId=data["data-event-id"]
+                    matchTime=data["data-timestamp"]
+                    teamA=data.select_one(".team-a").get_text().strip()
+                    score=data.select_one(".score-time").get_text().strip()
+                    
+                    if "P" in score:
+                        score="P"                        
+                    
+                    teamB=data.select_one(".team-b").get_text().strip()
+                    
+                    my_list.append((matchId, str(datetime.fromtimestamp(int(matchTime))), '이강인', teamA, score, teamB))                    
+                except Exception as e:
+                    pass
+            
+            if db:
+                con=pymysql.connect(user="root", passwd="shine20406!", host="localhost", db="secretnews", charset="utf8")
+                cur=con.cursor()
+                
+                sql="INSERT INTO smatch (mid, mtime, league, teama, score, teamb) VALUES (%s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE score=VALUES(score)"
+                cur.executemany(sql, my_list)
+                
+                now=time
+                print(now.strftime(matchId + " | " + '%Y-%m-%d %H:%M:%S') + " | " + str(cur.rowcount))
+                
+                cur.close()
+                con.commit()
+                con.close()
+            else:
+                print(my_list)
+        else:
+            print(sys._getframe().f_code.co_name + "=" + str(req.status_code))
+    except Exception as e:
+        now=time
+        print(now.strftime('%Y-%m-%d %H:%M:%S') + " | request exception | " + e)
+        
 def wc22(db):
     url="https://kr.soccerway.com/international/world/world-cup/2022-qatar/group-stage/r49519/matches/"
     
@@ -171,4 +223,5 @@ if __name__ == "__main__":
     # batch()
     # wctt(False)
     # wc22(True)
-    epl(False)
+    # epl(False)
+    mayo(True)
